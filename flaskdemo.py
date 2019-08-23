94e1b59bb7eb48b5d437722e93eaf257077a077f
murugaraju <murugan.iiit2326@gmail.com>
2019-08-23 21:06:58 +0530

    angular login animation practice

---
+++ flaskdemo.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_cors import CORS
+import time
+import json
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/bro')
+def hello():
+	
+
+    return json.dumps({"fruit":"apple"})
+
+if __name__ == '__main__':
+    app.run(port='4300')
